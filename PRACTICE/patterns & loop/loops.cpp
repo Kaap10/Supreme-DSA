3f38dc244193a66bfab2f9e904a28b95a1a9c20b
Kaap10 <vardhmangupta2004@gmail.com>
2023-10-18 23:18:02 +0530

    updated

--- PRACTICE/patterns & loop/loops.cpp
+++ PRACTICE/patterns & loop/loops.cpp
@@ -36,14 +36,69 @@ using namespace std;
 // return 0;
 // // }
 
-int main() {
-    int n=5;
+// int main() {
+//     int n=5;
 
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<(i+1); j++) {
+//     for(int i=0; i<n; i++) {
+//         for(int j=0; j<(i+1); j++) {
+//             cout << "* ";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+// int main() {
+//     int n=5;
+
+//     for (int i=0; i<n; i++) {
+//         for (int j=0; j<(n-i); j++) {
+//             cout << "* ";
+
+//         }
+//         cout <<endl;
+//     } 
+//     return 0;
+// }
+
+// int main() {
+//     int n=5;
+
+//     for (int i=0; i< n; i++) {
+//         for (int j=0; j<n; j++) {
+//             if (i==0 || i==n-1) {
+//                 cout << "* ";
+//             }
+//             else { 
+//                 if( j==0 || j==n-1) {
+//                     cout << "* ";
+//                 }
+//                 else {
+//                     cout << "  ";
+                
+//                 }
+//             }
+
+//         }
+//             cout << endl;
+//     }
+//     return 0;
+
+// }
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<(n-i-1); j++) {
+            cout << " ";
+        }
+
+        for (int j=0; j<(i+1); j++) {
             cout << "* ";
         }
-        cout << endl;
+        cout <<endl;
     }
     return 0;
 }
