508a2476e0c3bda9169cc34a83d331805caeaf2a
Kaap10 <vardhmangupta2004@gmail.com>
2023-10-18 13:47:27 +0530

    practice loops

---
+++ 🎯Practice/patterns & loop/loops.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+// int main() {
+//     for(int i=0; i<4; i++) {
+//         cout << "Kap10"<<i<<endl;;
+//     }
+
+//     return 0;
+// }
+
+//Pattern problem
+
+//pattern1 SQUARE 
+int main() {
+    int n=3;
+
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<(n+2); j++) {
+            cout << "* ";
+        }
+    cout <<endl;
+    }
+return 0;
+}
