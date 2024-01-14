c48921fb760d05592b4c83823348602c3621e99d
Kaap10 <vardhmangupta2004@gmail.com>
2024-01-14 20:23:37 +0530

    update 14/1/24

---
+++ Practice/14.cpp
@@ -0,0 +1,140 @@
+//Hello World likhna hai 
+#include<iostream>
+using namespace std;
+
+// int main() {
+//     //finding quotient & remainder of numbers taken by user
+//     int a;
+//     int b;
+
+//     cout << "First Number is: "<<endl;
+//     cin >> a;
+
+//     cout << "Seconf number is: "<<endl;
+//     cin >> b;
+
+//     //remainder nikalne ka formula -> divident ko divisior se divide krdo in this case a/b hojayega
+//     int quotient = a/b;
+
+//     //quotient nikalne ke liye -> % ka istemaal kiya jata hai formula -> a%b
+//     int remainder = a%b;
+
+//     cout << "Remainder of a and b is: "<<remainder<<endl;
+//     cout << "Quotient of a and b is: "<<quotient<<endl;
+//     return 0;
+// }
+
+//Size of variable nikalna ho toh->
+// int main() {
+//     // int n;
+//     // cin>>n;
+
+//     cout << "Size:"<<sizeof(int)<<endl;
+//     cout << "Size:"<<sizeof(float)<<endl;
+//     cout << "Size:"<<sizeof(char)<<endl;
+//     cout << "Size:"<<sizeof(double)<<endl;
+//     // cout << "Size of n is: "<<result<<endl;
+//     return 0;
+// }
+
+//swapping krni hai using third variable
+// int main() {
+//     int a;
+//     int b;
+//     int temp;
+//     cin >>a;
+//     cin >>b;
+//     cin >>temp;
+
+//     cout << "Before sawpping"<<endl;
+//     cout << "a=" <<a <<" b= "<<b<<endl;
+
+//     temp = a;
+//     a = b;
+//     b = temp;
+
+//     cout << "After swapping"<<endl;
+//     cout << "a="<<a<<" b= "<<b<<endl;
+//     return 0;
+// }
+
+//Mereko ASCII value chaiye kisi bhi ki
+// int main() {
+//     char c;
+//     cout << "enter the variable: ";
+//     cin>> c;
+
+//     cout << "ASCII value of  "<<c<<"is: "<<int(c)<<endl;
+//     return 0;
+// }
+
+//MULTIPLY TWO NUMBER number entered by user
+// int main() {
+//     int a;
+//     int b;
+
+//     cout << "Enter the First number: ";
+//     cin >> a;
+
+//     cout << "Enter the Second number: ";
+//     cin >>b;
+
+//     int mul = a*b;
+//     cout<< "Multiply of "<<a<<" & "<<b<<" is "<<mul<<endl;
+//     return 0;
+// }
+
+// int main() {
+//   int n;
+
+//   cout << "Enter an integer: ";
+//   cin >> n;
+
+//   if ( n % 2 == 0)
+//     cout << n << " is even.";
+//   else
+//     cout << n << " is odd.";
+
+//   return 0;
+// }
+
+//Given Input is Vowel or Consonants
+// int main() {
+//     char c;
+//     cout << "Enter the character: ";
+//     cin >> c;
+
+//     int Lower_case;
+//     int Upper_case;
+
+//     Lower_case = (c=='a' || c=='e' || c=='i'|| c=='o' || c=='u' );
+//     Upper_case = (c=='A' || c=='E' || c=='I'|| c=='O' || c=='U' );
+
+//     if(!isalpha(c)) {
+//         cout << "Error!"<<endl;
+//     }
+//     else if(Lower_case || Upper_case) {
+//         cout << c << " is a Vowel."<<endl;
+//     }
+//     else {
+//         cout << c << " is not a Vowel"<<endl;
+//     }
+// }
+
+//find the largest number
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
